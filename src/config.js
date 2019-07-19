cfa4a7f557be7b1a6e290fd1014260312aa74aa2
Weston Gibler <weston@bluekeel.com>
2019-07-19 09:59:19 -0600

    Configuring environments

--- src/config.js
+++ src/config.js
@@ -1,17 +1,44 @@
-export default {
-  MAX_ATTACHMENT_SIZE: 5000000,
+const dev = {
   s3: {
     REGION: "us-east-1",
-    BUCKET: "my-note-uploads"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-t0ckled39zwc"
   },
   apiGateway: {
     REGION: "us-east-1",
-    URL: "https://m6g7p6ukw5.execute-api.us-east-1.amazonaws.com/prod"
+    URL: "http://notes.bluekeel-api.com/dev"
   },
   cognito: {
     REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_OXEm7IlOL",
-    APP_CLIENT_ID: "io2ss9365lu8f22l5j71u4kev",
-    IDENTITY_POOL_ID: "us-east-1:6898e448-817f-494a-afd4-0c0c953db40c"
+    USER_POOL_ID: "us-east-1_KbnVpIWgD",
+    APP_CLIENT_ID: "62oost4nt5f33n07h49vshml8b",
+    IDENTITY_POOL_ID: "us-east-1:fe9f6260-b4f2-4387-a4ea-f14912376a42"
   }
+};
+
+const prod = {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-c4y843c47qlu"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "http://notes.bluekeel-api.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ii1ujjCZg",
+    APP_CLIENT_ID: "Y3f6qlgcissush1k8e8mcqemt04",
+    IDENTITY_POOL_ID: "us-east-1:af121ee6-d7ce-4eb2-9d55-4f5351ebc804"
+  }
+};
+
+// Default to dev if not set
+const config = process.env.REACT_APP_STAGE === 'prod'
+  ? prod
+  : dev;
+
+export default {
+  // Add common config values here
+  MAX_ATTACHMENT_SIZE: 5000000,
+  ...config
 };
